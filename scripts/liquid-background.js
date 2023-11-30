397a85742b338cce8df0d7ff72590267803e983b
Foksen <izholobov2004@gmail.com>
2023-12-01 02:50:32 +0300

    Liquid background

---
+++ scripts/liquid-background.js
@@ -0,0 +1,22 @@
+const ANIMATED_BLOBS = document.getElementsByClassName('liquid-background-blob-container-animated');
+
+function setDefaultPos(blob) {
+    blob.style.marginLeft = 0;
+    blob.style.marginTop = 0;
+}
+
+function setRandomPos(blob, minX, maxX, minY, maxY) {
+    let marginX = Math.floor(Math.random() * (maxX - minX + 1)) + minX;
+    let marginY = Math.floor(Math.random() * (maxY - minY + 1)) + minY;
+    blob.style.marginLeft = marginX + "px";
+    blob.style.marginRight = marginX + "px";
+    blob.style.marginTop = marginY + "px";
+    blob.style.marginBottom = marginY + "px";
+}
+
+for (let blob of ANIMATED_BLOBS) {
+    setDefaultPos(blob);
+    setInterval(() => {
+        setRandomPos(blob, -400, 400, -400, 400);
+    }, 5000);
+}
